3cb6a4ebebceebbe36c0d62125cddcacc4ed7bd3
Einai <81778449+einaidev@users.noreply.github.com>
2022-10-08 16:52:00 -0300

    Delete requests_ directory

--- requests_/__init__.py
+++
@@ -1,1 +0,0 @@
-from .sendRequest import *
--- requests_/models/channel/video.py
+++
@@ -1,7 +0,0 @@
-class Channel:
-    def __init__(self,**args) -> None:
-        self.args = args
-
-    @property
-    def name(self) -> str:
-        return self.args["name"]
