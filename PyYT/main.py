c40c9a70b8d6e16d9a7f16eebe9c3a712ed9becb
Einai <81778449+einaidev@users.noreply.github.com>
2022-10-13 17:04:31 -0300

    Update main.py

--- PyYT/main.py
+++ PyYT/main.py
@@ -19,19 +19,3 @@ class Main:
     def start(self) -> videoInfo:
         r = self.r()
         return r
-r = Main("https://www.youtube.com/watch?v=BaI0tiwrKYk").start
-print("Thumbnail: {0}\nTitle: {1}\nViews: {2}\nChannel: {3}\nLikes: {4}\nDate: {5}\nEmbed: {6}\n\nDescription:\n{7}\n\nGenre: {8}\nChannel Url: {9}\nDuration: {10}\nId: {11}\nComments Size: {12}\nAllowed Regions: {13}".format(
-    r.video.thumbnail,
-    r.video.title,
-    r.video.views,
-    r.channel.name,
-    r.video.likes,
-    ", ".join(r.video.date),
-    r.video.embed,
-    r.video.description,
-    r.video.genre,
-    r.channel.url,
-    r.video.duration,
-    r.video.id,
-    r.video.comments.size,
-    r.video.allowedRegions,))
